bdbc12d5acbf25773e8d709ad8f81751225db510
alexL <aleksandrlambert@gmail.com>
2024-06-19 14:30:48 +0700

    Фиксирует значения, которые необходимы для использования далее

---
+++ app.js
@@ -0,0 +1,18 @@
+// значения текстовых импутов
+
+const totalCost = document.getElementById('total-cost'),
+    anInitalFee = document.getElementById('an-inital-fee'),
+    creditTern = document.getElementById('credit-tern');
+
+// значение из range импутов (бегунки)
+
+const totalCostRange = document.getElementById('total-cost-range'),
+    anInitialFeeRange = document.getElementById('an-inital-fee-range'),
+    creditTernRange = document.getElementById('credit-tern-range');
+
+// итоговые значения
+
+const totalAmountOfCredit = document.getElementById('amount-of-credit'),
+    totalMounthlyPayment = document.getElementById('monthly-payment'),
+    totalRecommendedIncome = document.getElementById('recommended-income');
+
