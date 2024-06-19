fdef2207bdaccd9addd5e4de941bc05715fa9a8c
alexL <aleksandrlambert@gmail.com>
2024-06-19 17:25:21 +0700

    исправил опечатки, добавил массив и функцию вывода процентных значений

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
 // значения текстовых импутов
 
 const totalCost = document.getElementById('total-cost'),
-    anInitalFee = document.getElementById('an-inital-fee'),
-    creditTern = document.getElementById('credit-tern');
+    anInitialFee = document.getElementById('an-initial-fee'),
+    creditTerm = document.getElementById('credit-term');
 
 // значение из range импутов (бегунки)
 
 const totalCostRange = document.getElementById('total-cost-range'),
-    anInitialFeeRange = document.getElementById('an-inital-fee-range'),
-    creditTernRange = document.getElementById('credit-tern-range');
+    anInitialFeeRange = document.getElementById('an-initial-fee-range'),
+    creditTermRange = document.getElementById('credit-term-range');
 
 // итоговые значения
 
 const totalAmountOfCredit = document.getElementById('amount-of-credit'),
-    totalMounthlyPayment = document.getElementById('monthly-payment'),
+    totalMonthlyPayment = document.getElementById('monthly-payment'),
     totalRecommendedIncome = document.getElementById('recommended-income');
 
+// значения всех range
+
+const inputsRange = document.querySelectorAll('.input-range');
+
+// кнопки с процентной ставкой 
+
+const bankBtns = document.querySelectorAll('.bank');
+
+const assignValue = () => {
+    totalCost.value = totalCostRange.value;
+    anInitialFee.value = anInitialFeeRange.value;
+    creditTerm.value = creditTermRange.value;
+}
+
+assignValue();
+
+const banks = [
+    {
+        name: 'alfa',
+        percent: 8.7
+    },
+    {
+        name: 'sberbank',
+        percent: 8.4
+    },
+    {
+        name: 'pochta',
+        percent: 7.9
+    },                  
+    {
+        name: 'tinkoff',
+        percent: 9.2
+    }
+]
+
+let currentPrecent = banks[2].percent;
+console.log(currentPrecent);
